dd74300f88baf7164358926fabc92a333d7ac15f
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-02-17 17:55:14 +0100

    🔥 Remove unused imports
    Remove unused imports in the main class

--- src/application/App.java
+++ src/application/App.java
@@ -1,8 +1,6 @@
 import java.time.LocalDate;
 //import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
